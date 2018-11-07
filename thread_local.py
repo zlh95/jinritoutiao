3f0c4244b69bd798a5a22b3ac0cc04aa047e97a7
zlh95 <35726058+zlh95@users.noreply.github.com>
2018-11-07 17:01:39 +0800

    Update thread_local.py

--- thread_local.py
+++ thread_local.py
@@ -1,18 +1,42 @@
 from threading import Thread,get_ident
 
 class Local(object):
-    ident = get_ident()
+
     def __init__(self):
-        object.__setattr__(self,'storage',{})
+        self.storage = {}
+        self.get_ident = get_ident
 
-    def __setattr__(self, key, value):
-        if self.ident in self.storage:
-            self.storage[self.ident][key] = value
+    def set(self, k, v):
+        ident = self.get_ident()
+        origin = self.storage.get(ident)
+        if not origin:
+            origin = {k: v}
         else:
-            self.storage[self.ident] = {key:value}
+            origin[k] = v
+        self.storage[ident] = origin
+    def get(self, k):
+        ident = self.get_ident()
+        origin = self.storage.get(ident)
+        if not origin:
+            return None
+        else:
+            return origin.get(k, None)
 
-    def __getattr__(self, item):
-        return self.storage[self.ident][item]
+local_values = Local()
+
+
+def task(num):
+    local_values.set("name", num)  # set方法
+    import time
+    time.sleep(1)
+    print(local_values.get("name"), Thread().name)
+
+
+if __name__ == '__main__':
+    for i in range(20):
+        t = Thread(target=task, args=(i,), name="%s" % i)
+        t.start()
+
 
     
 #类似实现ThreadLocal结构，通过字典的方式，让每个线程保存的数据独立，互不影响。
