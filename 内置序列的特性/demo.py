c5843131183f0172ba5c0a8d9f429860e5303cec
zlh95 <346767586@qq.com>
2018-11-15 22:40:27 +0800

    go

---
+++ 内置序列的特性/demo.py
@@ -0,0 +1,3 @@
+1.容器序列：list,tuple,collections.deque,这些序列能存放不同类型的数据。
+2.扁平序列：str,bytes,bytearray,memoryview,array.array,这类序列只能存放一种类型。
+3.可变序列：list,bytearray,array.array,collections.deque,memoryview,所有标准库中的映射类都是可变的。
